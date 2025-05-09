25dd731ce677237497e643f754b8cb345a9d59d6
qndan <80284796+qndan@users.noreply.github.com>
2025-05-09 22:27:24 +0000

    add another AntimonyEditor.tsx test

--- src/__tests__/components/AntimonyEditor.test.tsx
+++ src/__tests__/components/AntimonyEditor.test.tsx
@@ -1,14 +1,14 @@
-// TODO: needs to test if annotated variable actions work
-
+import { readFile, readFileSync } from "fs";
 import * as monaco from "monaco-editor";
 import { act, render, RenderResult } from "@testing-library/react";
 import { useState } from "react";
 
 import AntimonyEditor, { processContent, AntimonyEditorProps } from "../../components/antimony-editor/AntimonyEditor";
 import { SrcPosition } from "../../language-handler/Types";
+import path from "path";
 
 describe("AntimonyEditor", () => {
-  // We have secret mock functions inside of the mock module.
+  // We have secret mock functions inside of the mock module. They aren't being used right now though.
   const monacoMock = monaco as any;
 
   const DEFAULT_FILES = [
@@ -19,7 +19,8 @@ describe("AntimonyEditor", () => {
       "A: lprll1p2r1lplp laplfsdf\n"
       + "model notes ```\n"
       + "\t\t<b>convert this to markdown</b>\n"
-      + "```";
+      + "\t```\n"
+      + "end";
 
   const createMockDatabase = (files: { name: string, content: string }[]) => {
     const objectStore = {
@@ -34,6 +35,13 @@ describe("AntimonyEditor", () => {
     return {
       transaction: () => transaction,
     }
+  };
+
+  const mockType = async (mockEditor: any, text: string) => {
+    await act(async () => {
+      mockEditor.setValue(text);
+      mockEditor.onDidChangeModelContent.mock.lastCall[0]();
+    });
   };
 
   const MockAntimonyEditor = (
@@ -123,11 +131,10 @@ describe("AntimonyEditor", () => {
 
     expect(mockMonacoEditor.getValue()).not.toContain("**convert this to markdown**");
 
-    await act(async () => {
-      mockMonacoEditor.setValue(ANTIMONY_WITH_NOTES);
-      mockMonacoEditor.onDidChangeModelContent.mock.lastCall[0]();
-    });
+    // Fake typing in the editor
+    mockType(mockMonacoEditor, ANTIMONY_WITH_NOTES);
 
+    // Now the HTML should be converted to markdown
     expect(mockMonacoEditor.getValue()).toContain("**convert this to markdown**");
   });
 
@@ -142,14 +149,17 @@ describe("AntimonyEditor", () => {
       />);
     });
 
+    // Replace the global alert function with a mock
     const originalAlert = global.alert;
     const mockAlert = jest.fn();
     global.alert = mockAlert;
 
+    // Call the action to create an annotation
     await act(async() => {
       mockMonacoEditor.mockRunAction("create-annotation");
     });
 
+    // It should error b/c they aren't hovering over a variable
     expect(mockAlert).toBeCalled();
 
     global.alert = originalAlert;
@@ -184,7 +194,6 @@ describe("AntimonyEditor", () => {
     let mockMonacoEditor: any;
     const files = [{ name: "test.ant", content: "not saved yet" }];
     const mockDatabase = createMockDatabase(files);
-    // doesn't work without `act` :(
     await act(async () => {
       antimonyEditor = render(<MockAntimonyEditor
         files={files}
@@ -193,11 +202,57 @@ describe("AntimonyEditor", () => {
       />);
     });
 
-    await act(async () => {
-      mockMonacoEditor.setValue("SAVE ME");
-      mockMonacoEditor.onDidChangeModelContent.mock.lastCall[0]();
-    });
+    // Fake typing
+    await mockType(mockMonacoEditor, "SAVE ME");
 
     expect(mockDatabase.transaction().objectStore().put).toBeCalledWith({name: "test.ant", content: "SAVE ME"});
+  });
+  
+  it("should navigate to annotation when hovering over one", async () => {
+    const biomodelContent = readFileSync(path.resolve(__dirname, "BIOMD00000000362.ant"), "utf8").toString();
+    let antimonyEditor: RenderResult;
+    let mockMonacoEditor: any;
+    const files = [{ name: "BIOMD00000000362.ant", content: "plz work" }];
+    const mockDatabase = createMockDatabase(files);
+    await act(async () => {
+      antimonyEditor = render(<MockAntimonyEditor
+        files={files}
+        mockDatabase={mockDatabase}
+        editorInstanceExtractor={e => mockMonacoEditor = e}
+      />);
+    });
+
+    // For whatever reason, editor doesn't initialize with the file contents, so we just type it out :(
+    await mockType(mockMonacoEditor, biomodelContent);
+
+    // Trick the editor into thinking they're hovering over a variable
+    mockMonacoEditor.getPosition = () => ({
+      lineNumber: 19,
+      column: 3,
+    });
+
+    const maxColumn = 80;
+    mockMonacoEditor.getModel = () => ({
+      getLineMaxColumn: () => maxColumn,
+      getWordAtPosition: () => ({
+        word: "ProteinD",
+        startColumn: 3,
+        endColumn: 11,
+      }),
+    });
+
+    // Now run the action to jump to the annotation
+    await act(async() => {
+      mockMonacoEditor.mockRunAction("Navigate to Edit Annotation");
+    });
+
+    // Should've selected the last annotation for ProteinD which is on line 66
+    expect(mockMonacoEditor.revealLineInCenter).toBeCalledWith(66);
+    expect(mockMonacoEditor.setSelection).toBeCalledWith({
+      startLineNumber: 66,
+      startColumn: 1,
+      endLineNumber: 66,
+      endColumn: maxColumn,
+    });
   });
 });
